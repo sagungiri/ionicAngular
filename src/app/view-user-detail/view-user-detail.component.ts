3d95b0251e1e4ddce4b11615fe2359ce23cac7e7
sagungiri <gireesagun998@gmail.com>
2021-02-19 19:13:12 +0545

    ok

--- src/app/view-user-detail/view-user-detail.component.ts
+++ src/app/view-user-detail/view-user-detail.component.ts
@@ -8,17 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class ViewUserDetailComponent implements OnInit {
 
   users = [];
+
   url = 'https://fir-angular-dc1a1-default-rtdb.firebaseio.com/data.json'
   constructor(private _http: Http) { }
 
   ngOnInit() {
     this._http.get(this.url).subscribe(res => {
       const data = res.json();
-      console.log('res', data)
-      //console.log('data', data)
-      this.users.push(data)
-    })
-    console.log('users', this.users)
+      console.log('res', data);
+
+
+      for (let i in data) {
+        console.log(i);
+        if (data.hasOwnProperty(i)) {
+          console.log('Key is: ' + i + '. Value is: ' + data[i]);
+          this.users.push(data[i]);
+        }
+      }
+    });
+    console.log('users', this.users);
   }
 
 }
