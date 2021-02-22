cfdb0654bdf72cc87dece29eb909755d7195a32f
sagungiri <gireesagun998@gmail.com>
2021-02-22 18:26:48 +0545

    ok

--- src/app/add-user-detail/add-user-detail.component.ts
+++ src/app/add-user-detail/add-user-detail.component.ts
@@ -14,7 +14,19 @@ export class AddUserDetailComponent implements OnInit {
     placeholder: '',
     tabsize: 2,
     height: '200px',
+    uploadImagePath: '/api/upload',
+    toolbar: [
+      ['misc', ['codeview', 'undo', 'redo']],
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+      ['fontsize', ['fontname', 'fontsize', 'color']],
+      ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
+      ['insert', ['table', 'picture', 'link', 'video', 'hr']]
+    ],
+    fontNames: ['Helvetica', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Roboto', 'Times']
   }
+
+
   url = 'https://fir-angular-dc1a1-default-rtdb.firebaseio.com/data.json'
   constructor(private http: Http,
     private router: Router) {
