c60e239d67787dab6e6c101627a67b099a7e848d
sagungiri <gireesagun998@gmail.com>
2021-02-25 16:23:19 +0545

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseService } from './services/firebase/firebase.service';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  isLoading=false;
+  @Output() isLogout = new EventEmitter<void>();
 
-  constructor( public menuCtrl: MenuController) { }
+  constructor(public firebaseService: FirebaseService,
+     public menuCtrl: MenuController,
+     private loadingCtrl: LoadingController,
+     private router: Router) { }
+
+  ngOnInit(){
+}
   toggleMenu() {
     this.menuCtrl.toggle(); 
+  }
+  logout = () => {
+    this.menuCtrl.toggle(); 
+    this.isLoading= true;
+    this.loadingCtrl.create({keyboardClose:true, message:'Logging Out.....'})
+    .then(loadingEl=>{
+      loadingEl.present();
+      setTimeout(()=>{
+        this.isLoading= false;
+        loadingEl.dismiss();
+      }, 2000);
+    })
+    this.firebaseService.logOut();
+    this.isLogout.emit();
+    this.router.navigateByUrl('/');
+
   }
 }
--- src/app/view-user-detail/view-user-detail.component.ts
+++ src/app/view-user-detail/view-user-detail.component.ts
@@ -1,8 +1,5 @@
-import { LoadingController } from '@ionic/angular';
-import { Router } from '@angular/router';
 import { Http } from '@angular/http';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { FirebaseService } from '../services/firebase/firebase.service';
+import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-view-user-detail',
   templateUrl: './view-user-detail.component.html',
@@ -12,13 +9,10 @@ export class ViewUserDetailComponent implements OnInit {
 
   users = [];
   isLoading=false;
-  @Output() isLogout = new EventEmitter<void>();
+
 
   url = 'https://fir-angular-dc1a1-default-rtdb.firebaseio.com/data.json'
-  constructor(private _http: Http,
-    public firebaseService: FirebaseService,
-    private router: Router,
-    private loadingCtrl: LoadingController) { }
+  constructor(private _http: Http) { }
 
   ngOnInit() {
     this._http.get(this.url).subscribe(res => {
@@ -36,19 +30,5 @@ export class ViewUserDetailComponent implements OnInit {
     });
     console.log('users', this.users);
   }
-  logout = () => {
-    this.isLoading= true;
-    this.loadingCtrl.create({keyboardClose:true, message:'Logging Out.....'})
-    .then(loadingEl=>{
-      loadingEl.present();
-      setTimeout(()=>{
-        this.isLoading= false;
-        loadingEl.dismiss();
-      }, 2000);
-    })
-    this.firebaseService.logOut();
-    this.isLogout.emit();
-    this.router.navigateByUrl('/');
 
-  }
 }
